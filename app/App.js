275dbeef7a6ad9b92fb47e26ed4d7b8fdad23c5b
andela-gukpere <godson.ukpere@andela.co>
2016-06-09 03:03:24 +0100

    chore(Base files): Update content of base files

--- app/App.js
+++ app/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Home from './components/Home.jsx';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
@@ -13,7 +12,7 @@ export default class App extends React.Component {
   render() {
     injectTapEventPlugin();
     return (
-      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+      <MuiThemeProvider muiTheme={getMuiTheme()}>
         <Home />
       </MuiThemeProvider>
     );
