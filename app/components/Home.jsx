bc023509920799af93ae5a84bf2f260f30b2cabb
andela-gukpere <godson.ukpere@andela.co>
2016-06-10 14:04:29 +0100

    feature(Sections): Use Paper components for sections

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -6,6 +6,7 @@ import SocialMedia from './shared/SocialMedia.jsx';
 import Proficiency from './shared/Proficiency.jsx';
 import Portfolio from './shared/Portfolio.jsx';
 import Footer from './shared/Footer.jsx';
+import Paper from 'material-ui/Paper';
 
 // Data imports
 import firebase from '../data/db.js';
@@ -29,6 +30,10 @@ export default class Home extends React.Component {
     this.getData('opensource');
   }
 
+  getZDepth() {
+    return 5;
+  }
+
   getData(key) {
     firebase.database.ref(key).on('value', (snap) => {
       if (snap.val()) {
@@ -50,7 +55,7 @@ export default class Home extends React.Component {
           <section className={styles.section}>
             <h1 className={styles.headerTitle}>Godson Ukpere</h1>
           </section>
-          <section className={classnames(styles.section, styles.sectionText)}>
+          <Paper className={classnames(styles.section, styles.sectionText)} rounded zDepth={this.getZDepth()}>
             <h4>Über / About</h4>
             <p><small>I'm a trained Electrical Engineer (B.Eng) with a passion for calisthenics. I've written <i>production</i> code for {this.getProductionYears()} years. Calisthenics und Javascript über alle.</small></p>
             <br/>
@@ -66,23 +71,23 @@ export default class Home extends React.Component {
             <p><i className={classnames('fa', 'fa-envelope-o', styles.marginRight)}></i><a href='mailto:godson.ukpere@gmail.com'>godson.ukpere@gmail.com</a></p>
             <p><i className={classnames('fa', 'fa-phone', styles.marginRight)}></i><a href='tel:+234-809-613-2990'>+234-809-613-2990</a></p>
             <p><i className={classnames('fa', 'fa-skype', styles.marginRight)}></i><a href='skype:godson.ukpere'>godson.ukpere</a></p>
-          </section>
-          <section className={styles.section} id="socialmedia">
+          </Paper>
+          <Paper className={styles.section} id="socialmedia" rounded zDepth={this.getZDepth()}>
             <h4>Sozial / Social</h4>
-            {this.state.socialmedia ? <SocialMedia items={this.state.socialmedia}/> : <CircularProgress color="#fff" size={1} />}
-          </section>
-          <section className={styles.section} id="proficiency">
+            {this.state.socialmedia ? <SocialMedia items={this.state.socialmedia}/> : <CircularProgress color="#009090" size={1} />}
+          </Paper>
+          <Paper className={styles.section} id="proficiency" rounded zDepth={this.getZDepth()}>
             <h4>Können / Proficiency</h4>
-            {this.state.proficiency ? <Proficiency items={this.state.proficiency}/> : <CircularProgress color="#fff" size={1} />}
-          </section>
-          <section className={styles.section} id="portfolio">
+            {this.state.proficiency ? <Proficiency items={this.state.proficiency}/> : <CircularProgress color="#009090" size={1} />}
+          </Paper>
+          <Paper className={styles.section} id="portfolio" rounded zDepth={this.getZDepth()}>
             <h4>Portefeuille / Portfolio</h4>
-            {this.state.portfolio ? <Portfolio items={this.state.portfolio} /> : <CircularProgress color="#fff" size={1} />}
-          </section>
-          <section className={styles.section} id="opensource">
+            {this.state.portfolio ? <Portfolio items={this.state.portfolio} /> : <CircularProgress color="#009090" size={1} />}
+          </Paper>
+          <Paper className={styles.section} id="opensource" rounded zDepth={this.getZDepth()}>
             <h4>Open Source</h4>
-            {this.state.opensource ? <Portfolio items={this.state.opensource}/> : <CircularProgress color="#fff" size={1} />}
-          </section>
+            {this.state.opensource ? <Portfolio items={this.state.opensource}/> : <CircularProgress color="#009090" size={1} />}
+          </Paper>
         </main>
         <Footer />
       </div>
