9597af99b23c0ac1fb5c11678195ddfb167a2114
andela-gukpere <godson.ukpere@andela.co>
2016-06-10 15:37:49 +0100

    feature(Favico): Add favico to App

--- app/main.js
+++ app/main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 import backgroundImage from 'file!./images/background.png';
+import {} from 'file?name=favicon.ico!./favicon.ico';
 
 document.body.style.backgroundImage = `url("${backgroundImage}")`;
 ReactDOM.render(<App />, document.getElementById('root'));
