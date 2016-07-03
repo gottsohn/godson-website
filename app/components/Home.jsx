6cd3bc45e3e47a13b849a3c65a5d2af312af570c
Godson Ukpere <godson.ukpere@gmail.com>
2016-07-03 18:56:30 +0100

    chore(Loading): Use Loading compoennt for progress status

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import classnames from 'classnames';
-import CircularProgress from 'material-ui/CircularProgress';
 import Paper from 'material-ui/Paper';
-import styles from '../App.css';
 
+import styles from '../App.css';
 import SocialMedia from './shared/SocialMedia.jsx';
 import Proficiency from './shared/Proficiency.jsx';
 import Portfolio from './shared/Portfolio.jsx';
 import Footer from './shared/Footer.jsx';
 import HomeSection from './shared/HomeSection.jsx';
+import Loading from './shared/Loading.jsx';
 import firebase from '../database';
 
 export default class Home extends React.Component {
@@ -34,7 +34,7 @@ export default class Home extends React.Component {
     this.getData('content');
   }
 
-  getZDepth = () => 3
+  getZDepth = () => 2
 
   getData(key) {
     firebase.database.ref(key).on('value', (snap) => {
@@ -59,27 +59,27 @@ export default class Home extends React.Component {
           <Paper className={styles.section} id="socialmedia" rounded zDepth={this.getZDepth()}>
             <h4><p><small>{this.state.content? this.state.content.data.socialmedia.title : null}</small></p></h4>
             <p><small>{this.state.content? this.state.content.data.socialmedia.text : null}</small></p>
-            {this.state.socialmedia ? <SocialMedia items={this.state.socialmedia}/> : <CircularProgress color="#009090" size={0.5} />}
+            {this.state.socialmedia ? <SocialMedia items={this.state.socialmedia}/> : <Loading />}
           </Paper>
           <Paper className={styles.section} id="proficiency" rounded zDepth={this.getZDepth()}>
             <h4><p><small>{this.state.content? this.state.content.data.proficiency.title : null}</small></p></h4>
             <p><small>{this.state.content? this.state.content.data.proficiency.text : null}</small></p>
-            {this.state.proficiency ? <Proficiency items={this.state.proficiency}/> : <CircularProgress color="#009090" size={0.5} />}
+            {this.state.proficiency ? <Proficiency items={this.state.proficiency}/> : <Loading />}
           </Paper>
           <Paper className={styles.section} id="portfolio" rounded zDepth={this.getZDepth()}>
             <h4><p><small>{this.state.content? this.state.content.data.portfolio.title : null}</small></p></h4>
             <p><small>{this.state.content? this.state.content.data.portfolio.text : null}</small></p>
-            {this.state.portfolio ? <Portfolio items={this.state.portfolio} /> : <CircularProgress color="#009090" size={0.5} />}
+            {this.state.portfolio ? <Portfolio items={this.state.portfolio} /> : <Loading />}
           </Paper>
           <Paper className={styles.section} id="opensource" rounded zDepth={this.getZDepth()}>
             <h4><p><small>{this.state.content? this.state.content.data.opensource.title : null}</small></p></h4>
             <p><small>{this.state.content? this.state.content.data.opensource.text : null}</small></p>
-            {this.state.opensource ? <Portfolio items={this.state.opensource}/> : <CircularProgress color="#009090" size={0.5} />}
+            {this.state.opensource ? <Portfolio items={this.state.opensource}/> : <Loading />}
           </Paper>
           <Paper className={styles.section} id="developer-account" rounded zDepth={this.getZDepth()}>
             <h4><p><small>{this.state.content? this.state.content.data.developerAccount.title : null}</small></p></h4>
             <p><small>{this.state.content? this.state.content.data.developerAccount.text : null}</small></p>
-            {this.state.developerAccount ? <Portfolio items={this.state.developerAccount}/> : <CircularProgress color="#009090" size={0.5} />}
+            {this.state.developerAccount ? <Portfolio items={this.state.developerAccount}/> : <Loading />}
           </Paper>
         </main>
         <Footer />
