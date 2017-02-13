d07192a63d114d421af3a10033f479c184f238db
Godson Ukpere <godson.ukpere@gmail.com>
2017-02-13 20:50:30 +0100

    chore(Main): Remove background image

--- app/main.jsx
+++ app/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-import backgroundImage from 'file!./images/background.png';
+// import backgroundImage from 'file!./images/background.png';
 import {} from 'file?name=favicon.ico!./favicon.ico';
 
-document.body.style.backgroundAttachment = 'fixed';
-document.body.style.backgroundImage = `url("${backgroundImage}")`;
+// document.body.style.backgroundAttachment = 'fixed';
+// document.body.style.backgroundImage = `url("${backgroundImage}")`;
 ReactDOM.render(<App />, document.getElementById('root'));
