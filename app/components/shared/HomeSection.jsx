6fdae01735ca69bfdfd112dc22a39d247d9e35d7
Godson Ukpere <godson.ukpere@gmail.com>
2017-02-14 14:04:42 +0100

    chore(HomeSection): Replace img with unicode emoji chars

--- app/components/shared/HomeSection.jsx
+++ app/components/shared/HomeSection.jsx
@@ -19,8 +19,8 @@ export default class HomeSection extends React.Component {
     return (
       <div>
         <h4>About</h4>
-        <p><small>I've written production code for <b data-tip={this.getProductionYears.toString()}>{this.getProductionYears()}</b> years with solid Front End background and a passion for calisthenics <img className={styles.emoji} data-tip="Ich haben abdominal muscles" src="http://emojipedia-us.s3.amazonaws.com/cache/4a/d7/4ad7acf1055461c7bee61952af7b35e4.png"/>. I work with NodeJS, PHP and Python servers, Angular or React JS for front end, native mobile SDKs (Java / Android and Swift / iOS), with soft skills of an astute Human Resource Manager <img className={styles.emoji} data-tip="Boss" src="http://emojipedia-us.s3.amazonaws.com/cache/fe/9f/fe9fd3657ed6faa5319f5357b2de48eb.png"/>. For UI/UX, <Anchor href="https://material.google.com/" label="Material Design"/> is bae.</small></p>
-        <p><img className={styles.emoji} src="http://emojipedia-us.s3.amazonaws.com/cache/10/46/1046faae6fca73fff175423593ed5ef0.png"/> Calisthenics und JavaScript über alle.</p>
+        <p><small>I've written production code for <b data-tip={this.getProductionYears.toString()}>{this.getProductionYears()}</b> years with solid Front End background and a passion for calisthenics <span className={styles.emoji} data-tip="Ich haben abdominal muscles">🍫</span>. I work with NodeJS, PHP and Python servers, Angular or React JS for front end, native mobile SDKs (Java / Android and Swift / iOS), with soft skills of an astute Human Resource Manager <span className={styles.emoji}>👋</span>. For UI/UX, <Anchor href="https://material.google.com/" label="Material Design"/> is bae.</small></p>
+        <p><span className={styles.emoji}>🇩🇪</span> Calisthenics und JavaScript über alle.</p>
         <br/>
         <h4>Contact</h4>
         <p><small><i className={classnames('fa', 'fa-envelope-o', styles.marginRight)}></i><a href='mailto:godson.ukpere@gmail.com'>godson.ukpere@gmail.com</a></small></p>
